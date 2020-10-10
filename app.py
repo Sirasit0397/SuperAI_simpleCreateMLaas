739dade0d9f727877f1d5d5a59514d0173b3104e
Sirasit0397 <71720056+Sirasit0397@users.noreply.github.com>
2020-10-10 12:18:56 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+import joblib
+import numpy as np
+#import re
+ 
+app = Flask(__name__)
+CORS(app)
+ 
+@app.route('/')
+def helloworld():
+    return 'Helloworld'
+ 
+@app.route('/area')
+@cross_origin()
+def area():
+    w = float(request.values['w'])
+    h = float(request.values['h'])
+    return str(w*h)
+ 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=False)
+
+@app.route('/bmi', methods=['GET'])
+@cross_origin()
+def bmi():
+    kg = float(request.values['kg'])
+    m = float(request.values['m'])
+    return str(kg/((m/100)*(m/100)))
+
+@app.route('/iris', methods=['POST'])
+@cross_origin()
+def predict_species():
+    model = joblib.load('iris.model')#loadmodel
+    req = request.values['param']
+    inputs = np.array(req.split(','), dtype=np.float32).reshape(1,-1)
+    predict_target = model.predict(inputs)
+    if predict_species == 0:
+        return 'Sentosa'
+    elif predict_species == 1:
+        return 'Versicolour'
+    else:
+        return 'Virginica'        
+
+ 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)    
